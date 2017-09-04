cd5b05ac3c36e2517f8cc3699778d805aca5bc1d
Michelle Steele <michelle.steele@groundsure.com>
2017-09-04 16:58:09 +0100

    Show a noise map - early progress

--- render/app.js
+++ render/app.js
@@ -28,39 +28,69 @@ if (mapnik.register_default_input_plugins) mapnik.register_default_input_plugins
 mapnik.register_default_fonts();
 
 var map = new mapnik.Map(600, 400);
-map.srs= "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
+
+var srs = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs"
+
+map.srs= srs;
+
+// 
+// read in input
+//
+noise = require("../noisedata.json");
+// console.log(noise['datasets'][0]['geojson']);
+
+// 
+// calc extents
+//
+// TODO: calculate extents (lax, lay etc. below) - use GDAL?
 
 // 
 // Create background layer
 //
 var bg_layer = new mapnik.Layer("bg_lyr");
 
-// 337466.24015247775, 362106.6933502753, 337730.24015247775, 362370.6933502753)
-bg_layer.datasource = new mapnik.Datasource({ 'type': 'raster', 'file': 'bg_map.png', 
-  'lox': 337500.24015247775, 'loy': 362206.6933502753, 'hix': 337700.24015247775, 'hiy': 362300.6933502753 });
-bg_layer.srs= "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
+bg_layer.datasource = new mapnik.Datasource({ 'type': 'raster', 'file': 'noise_bg.png', 
+//'lox': 337500.24015247775, 'loy': 362206.6933502753, 'hix': 337700.24015247775, 'hiy': 362300.6933502753 });
+'lox': 511351.7435144611, 'loy': 220977.96466369674, 'hix': 511688.0605144609, 'hiy': 221314.28166369654});
+bg_layer.srs=srs;
 bg_layer.styles=["background_map"];
+
+map.add_layer(bg_layer);
+
+///
+// Create noise layer
+//
+
+var dataset = noise['datasets'][0]
+var geojson = dataset['geojson'];
+
+var noise_layer = new mapnik.Layer("noise");
+console.log(JSON.stringify(geojson));
+noise_layer.datasource = new mapnik.Datasource({
+  'type': 'ogr',
+  'layer': 'OGRGeoJSON',
+  'string': JSON.stringify(geojson)
+});
+noise_layer.srs = srs;
+noise_layer.styles=[dataset['dataset']]
+
+map.add_layer(noise_layer);
 
 //
 // Create site outline layer
 //
 
-var site = "Polygon((337581 362223, 337581 362223, 337581 362223, 337583 362238, 337584 362240, 337584 362245, 337585 362250, 337586 362260, 337588 362260, 337593 362260, 337597 362260, 337598 362260, 337602 362260, 337605 362261, 337608 362261, 337609 362261, 337611 362241, 337616 362242, 337617 362226, 337595 362217, 337590 362219, 337585 362221, 337581 362223))";
+
+var site = noise['site']
 
 var site_outline = new mapnik.Layer("site_outline");
 site_outline.datasource = new mapnik.Datasource({
   'type': 'csv',
   'inline': 'id,wkt\n1,"' + site + '"\n'
 });
-site_outline.srs= "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
-site_outline.styles=["site_outline", "defra_noise_rail_lden"];
+site_outline.srs=srs;
+site_outline.styles=["site_outline"];
 
-
-// 
-// Add layers to map
-//
-
-map.add_layer(bg_layer);
 map.add_layer(site_outline);
 
 //
@@ -68,6 +98,9 @@ map.add_layer(site_outline);
 //
 
 map.loadSync(stylesheet);
+
+
+//  TODO: zoom to extents only
 map.zoomAll();
 map.renderFileSync(image);
 
